322decec3ed8991dea0bc0326c8c5832e8de7ae6
Sebastian Reimers <hallo@studio-link.de>
2019-09-17 21:50:34 +0200

    bump flac and openssl version

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -1,9 +1,9 @@
 vmajor=19; vminor=9; vpatch=0
 opus="1.3.1"
-openssl="1.1.1c"
-openssl_sha256="f6fb3079ad15076154eda9413fed42877d668e7069d9b87396d0804fdb3f4c90"
+openssl="1.1.1d"
+openssl_sha256="1e3a91bc1f9dfce01af26026f856e064eab4c8ee0a8f457b5ae30b40b8b711f2"
 rtaudio="master"
-flac="1.3.2"
+flac="1.3.3"
 github_org="https://github.com/Studio-Link"
 vstsdk="vstsdk367_03_03_2017_build_352"
 debug="RELEASE=1"
