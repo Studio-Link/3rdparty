b941b2eee9ba20e9b9cf6a9539958997bf85b301
Sebastian Reimers <hallo@studio-link.de>
2019-09-17 22:20:33 +0200

    revert openssl upgrade - pthread windows build failure

--- dist/lib/versions.sh
+++ dist/lib/versions.sh
@@ -1,12 +1,6 @@
-vmajor=19; vminor=9; vpatch=0
+github_org="https://github.com/Studio-Link"
 opus="1.3.1"
-openssl="1.1.1d"
-openssl_sha256="1e3a91bc1f9dfce01af26026f856e064eab4c8ee0a8f457b5ae30b40b8b711f2"
+openssl="1.1.1c"
+openssl_sha256="f6fb3079ad15076154eda9413fed42877d668e7069d9b87396d0804fdb3f4c90"
 rtaudio="master"
 flac="1.3.3"
-github_org="https://github.com/Studio-Link"
-vstsdk="vstsdk367_03_03_2017_build_352"
-debug="RELEASE=1"
-if [ -z "${TRAVIS_BRANCH+x}" ]; then
-TRAVIS_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-fi
