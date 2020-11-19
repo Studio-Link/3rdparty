48988e120fccf161d8b6e5e6790faa903ec00462
Sebastian Reimers <hallo@studio-link.de>
2020-11-19 11:16:35 +0100

    wip

--- dist/lib/functions.sh
+++ dist/lib/functions.sh
@@ -6,8 +6,6 @@ sl_prepare_version() {
 
 sl_prepare() {
     echo "start build on $BUILD_TARGET ($BUILD_OS)"
-    sed_opt="-i"
-
     sl_prepare_version
 
     mkdir -p build;
@@ -24,11 +22,6 @@ sl_get_openssl() {
         ${SHASUM} -a 256 -c -
     tar -xzf openssl-${openssl}.tar.gz
     ln -s openssl-${openssl} openssl
-    pushd openssl
-    # fix/patch openssl 1.1.1d bug
-    #wget https://github.com/openssl/openssl/commit/c3656cc594daac8167721dde7220f0e59ae146fc.diff
-    #patch --ignore-whitespace -p1 < c3656cc594daac8167721dde7220f0e59ae146fc.diff
-    popd
 }
 
 sl_get_flac() {
