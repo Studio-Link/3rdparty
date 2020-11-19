cf398e5368e7b60eadc6730474cea9ecc4fdc83c
Sebastian Reimers <hallo@studio-link.de>
2020-11-19 11:17:52 +0100

    Add windows support for github actions and cleanup (#2)

--- dist/lib/functions.sh
+++ dist/lib/functions.sh
@@ -6,13 +6,12 @@ sl_prepare_version() {
 
 sl_prepare() {
     echo "start build on $BUILD_TARGET ($BUILD_OS)"
-    sed_opt="-i"
-
     sl_prepare_version
 
     mkdir -p build;
     pushd build
-    mkdir -p my_include
+    mkdir -p sl_include
+    mkdir -p sl_lib
 
     SHASUM=$(which shasum)
 }
@@ -23,31 +22,12 @@ sl_get_openssl() {
         ${SHASUM} -a 256 -c -
     tar -xzf openssl-${openssl}.tar.gz
     ln -s openssl-${openssl} openssl
-    pushd openssl
-    # fix/patch openssl 1.1.1d bug
-    #wget https://github.com/openssl/openssl/commit/c3656cc594daac8167721dde7220f0e59ae146fc.diff
-    #patch --ignore-whitespace -p1 < c3656cc594daac8167721dde7220f0e59ae146fc.diff
-    popd
 }
 
 sl_get_flac() {
     wget https://ftp.osuosl.org/pub/xiph/releases/flac/flac-${flac}.tar.xz
     tar -xf flac-${flac}.tar.xz
     ln -s flac-${flac} flac
-}
-
-sl_get_rtaudio() {
-    wget https://github.com/Studio-Link/rtaudio/archive/${rtaudio}.tar.gz
-    tar -xzf ${rtaudio}.tar.gz
-    wget https://github.com/Studio-Link/rtaudio/compare/master...coreaudio.diff
-    #wget https://github.com/Studio-Link/rtaudio/compare/master...pulseaudio.diff
-    pushd rtaudio-${rtaudio}
-    #patch --ignore-whitespace -p1 < ../master...pulseaudio.diff
-    patch --ignore-whitespace -p1 < ../master...coreaudio.diff
-    popd
-    ln -s rtaudio-${rtaudio} rtaudio
-    cp -a rtaudio-${rtaudio}/rtaudio_c.h my_include/
-    rm -f ${rtaudio}.tar.gz
 }
 
 sl_get_soundio() {
@@ -65,7 +45,7 @@ sl_get_soundio() {
     fi
     popd
     ln -s libsoundio-master soundio
-    cp -a libsoundio-master/soundio my_include/
+    cp -a libsoundio-master/soundio sl_include/
     cp -a ../dist/windows/soundio/toolchain.cmake soundio/
     rm soundio.tar.gz
 }
